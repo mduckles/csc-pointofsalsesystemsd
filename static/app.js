dcc4583fd76cfaacccce67c7ae094b5123d828d2
mduckles <micah@duckles.org>
2022-03-23 14:17:37 +1300

    the front end you can now add what you are ordering

--- static/app.js
+++ static/app.js
@@ -1,11 +1,35 @@
-$( function() {
-    $( "#draggable" ).draggable();
-    $( "#droppable" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
+count = {}
+//function cancelbutton(){
+  //for((key,value) in count){
+    //count[key] = 0
+    //document.getElementById(count[value]).innerHTML = 0;
+
+  //}
+
+
+//};
+
+function plusbutoon(y){
+  if(y in count){
+  count[y] += 1
+  }
+  else{
+    count[y] = 1
+  }
+  document.getElementById(y).innerHTML = count[y];
+};
+
+function minusbutton(a){
+  
+    if(a in count){
+      if(count[a] >= 1){
+      count[a] -= 1
       }
-    });
-  } );
+      }
+      else{
+        count[a] = 0
+      }
+  
+    document.getElementById(a).innerHTML = count[a];
+
+};
