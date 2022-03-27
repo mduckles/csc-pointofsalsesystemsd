1d9bf236d95f41086e434a7cd85627606835163f
mduckles <micah@duckles.org>
2022-03-28 10:31:33 +1300

    implemented the the submit button to send back to the back end or flask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from menu import Menu
-from flask import Flask, render_template, send_from_directory
-
+from flask import Flask, render_template, send_from_directory, request, jsonify
+# instalizes flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,20 +8,46 @@ def myroot():
     #m = Menu()
     #return m.ReadMenu(),m.ReadDrinks()
     return " hello"
-
+# sets up the page /menu/
 @app.route('/menu/')
 def show_menu():
+    '''gets menu from the csv and then renders the html template'''
+    # gets the data from the csv files with the menu class
     m = Menu()
+    # menu in dict form
     menu = m.ReadMenu()
+    # drink menu in dict form
     a = m.ReadDrinks()
-    print(a)
+    # renders a template so I can use jinja in html
     return render_template('menu.html', menu=menu, title="Cafe",drinkmenu = a, nothing = "" )
     #return m.ReadMenu(),m.ReadDrinks()
-
+# lets us use app.js and styles.css
 @app.route('/static/<path:path>')
 def send_static(path):
+    '''make sure the templates can find the js and css files'''
     return send_from_directory('static', path)
     
+@app.route('/orders/')
+def orderspage():
+    '''renders the template for the orders.html'''
+    return render_template('orders.html')
+
+@app.route('/order',methods=['POST'])
+def orders():
+    '''recives the data inputed to the menu page'''
+    data = request.get_json()
+    data["Name"] = data["Name"].lower()
+    data["Name"] = data["Name"].replace(" ","")
+    m = Menu()
+    m.Orderwrite(data,data["Name"])
+    return jsonify(data)
+
+@app.route("/orders/name",methods=["POST"])
+def ordersname():
+    '''gets the name of the persons geting there order and send the display for them'''
+    name = request.get_json()
+    print(name)
+    return True
 
 if __name__ == "__main__":
     app.run()
