3de5a829613043f0250cd301cbe9b3870a06c432
mduckles <micah@duckles.org>
2022-03-28 10:33:12 +1300

    made the cancel and one submit button wrork and send to the back end

--- static/app.js
+++ static/app.js
@@ -1,13 +1,23 @@
 count = {}
-//function cancelbutton(){
-  //for((key,value) in count){
-    //count[key] = 0
-    //document.getElementById(count[value]).innerHTML = 0;
 
-  //}
-
-
-//};
+function submitbutton(){
+  a = document.getElementById("Name").value
+  count["Name"] = a
+  for(const [key, value] of Object.entries(count)){
+    if(value == 0){
+      delete count[key],count[value]
+    }
+  }
+  $.ajax({
+    type:"POST",
+    data: JSON.stringify(count),
+    url:"/order",
+    contentType: "application/json",
+    dataType: "json"
+  })
+  console.log(a)
+  console.log(count)
+}
 
 function plusbutoon(y){
   if(y in count){
@@ -17,13 +27,14 @@ function plusbutoon(y){
     count[y] = 1
   }
   document.getElementById(y).innerHTML = count[y];
+  console.log("hello")
 };
 
 function minusbutton(a){
   
     if(a in count){
       if(count[a] >= 1){
-      count[a] -= 1
+        count[a] -= 1
       }
       }
       else{
@@ -33,3 +44,27 @@ function minusbutton(a){
     document.getElementById(a).innerHTML = count[a];
 
 };
+function cancelbutton(){
+    for(const [key, value] of Object.entries(count)){
+      if(value >= 1){
+        count[key] = 0
+        document.getElementById(key).innerHTML = 0;
+      }
+      if(key == "Name"){
+        ducument.getElementById("Name").value ="";
+      }
+  }
+};
+
+function orderssubmit(){
+  x = document.getElementById("OrderName").value;
+  $.ajax({
+    type:"POST",
+    data: JSON.stringify(x),
+    url:"/order/name",
+    contentType: "application/json",
+    dataType: "json"
+  })
+
+};
+
