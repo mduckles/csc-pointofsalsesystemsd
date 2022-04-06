797e49babd7e94e110f6e01b5509a4691efd1573
mduckles <micah@duckles.org>
2022-04-06 21:44:37 +1200

    made options show up make it so you must enter  a name and a optons for thing with options

--- static/app.js
+++ static/app.js
@@ -1,32 +1,50 @@
-//var that takes all the data from the menu site 
-var count = {}
-var data = {}
+//var that storse the count for the amount of one thing orders on the front end
+var count={}
+// stores all the order data
+var data={}
+// stores all the orders to be sent to the kitchen
+var kitchen_data=[]
 
+// runs when the page starts
 $(document).ready(function(){
+  // hides the  options radios when 0 are orders
   $(".options").hide()
+  $(".cost_options").hide()
+  for (i in kitchen_data){
+    for (index in kitchen_data[i]){
+    $("#cooks").append('<p>'+kitchen_data[i][index]+'</p>');
+    }
+      }
 });
+function prosessdata2(datas){
+  kitchen_data[-1] = datas
+};
 
-
-
+// when submitbutton is pressed sends all data to the back end
 function submitbutton(){
-  // when submitbutton is pressed sends all data to the back end
-  a = $("#Name")
-  data["Name"] = a
+  // gets the data of order name
+  a = document.getElementById("orderName").value
+  // gives data the order name
+  data["ordername"] = a
+  $("input[type=radio]").prop('checked', false);
+  // post to /order in the back end
   $.ajax({
     type:"POST",
     data: JSON.stringify(data),
     url:"/order",
     contentType: "application/json",
-    dataType: "json"
+    dataType: "json",
+    // runs when the data was sent successfuly
+    success: function(datas){
+      prosessdata2(datas)
+    }
   })
-
 }
-
-lowerunder = function(stin) { 
+function lowerunder(stin) { 
+  // lowercases any string data and replaces space with _
     return stin.toLowerCase().replace(" ", "_")
 }
-
-function plusbutoon(name,options,cost_options) { // y = name; x = option names
+function plusbutoon(name,options="",cost_options) { // y = name; x = option names
   // when the plus button is presed ad one to the amout to order that item
   if(name in count){
     count[name] += 1
@@ -34,31 +52,49 @@ function plusbutoon(name,options,cost_options) { // y = name; x = option names
   else{
     count[name] = 1
   }
-//  for(i=1;i<= count[name];i++ ){
- //     data[name+i] = {} // fill empty object for each of the menu items
- // }
-      
+ for(i=1;i<= count[name];i++ ){
+   if(data.hasOwnProperty([name+i]) == false || data[name+i] == NaN){
+      data[name+i] = {} // fill empty object for each of the menu items
+      if(data[name+i].hasOwnProperty('Cost')==false){
+      data[name+i]['Cost'] = ""
+      }
+      if(data[name+i].hasOwnProperty('Options')== false){
+        data[name+i]['Options'] = ""
+      }
+   }
+  } 
   document.getElementById(name).innerHTML = count[name]; // make number appear on page
   lower_name = name.toLowerCase().replaceAll(" ", "_")
-
+  // runs through each options
   for( i in options){
-      options_lower = options[i].toLowerCase().replaceAll(" ",'_')
+      options_lower = options[i].toLowerCase().replaceAll(" ","_")
+      // clones the options when 
       lable = $("#"+options_lower+"lable").clone().attr('id', options_lower +"lable"+ count[name])
-      input = $("#"+options_lower+"input").clone().attr('id', options_lower +"input"+ count[name])
+      input = $("#"+options_lower+"input").clone().attr('id', options_lower +"input"+ count[name]).attr('name',lower_name+'options'+count[name]).prop('required',true);
 
       console.log("#"+"menuoptions"+lower_name)
+      // append to the thing they are options of
       $("#"+"menuoptions"+lower_name).append(lable)
       $("#"+"menuoptions"+lower_name).append(input)
       $("#"+options_lower+"lable"+ count[name]).show()
-      $("#"+options_lower+"input"+ count[name]).show()
-
+      $("#"+options_lower+"input"+ count[name]).show().prop('checked',true)
     }
-  
+    for(i in cost_options){
+      // cones and appends to what ever the meal is the cost options
+      cost_lower = cost_options[i].toLowerCase().replaceAll(" ","_").replaceAll("$","").replaceAll(".","")
+      console.log("#"+lower_name+cost_lower+"lable")
+      lable_cost = $("#"+lower_name+cost_lower+"lable").clone().attr('id',lower_name+cost_lower+"lable"+ count[name])
+      $("#"+"menuoptions"+lower_name).append(lable_cost)
+      $("#"+lower_name+cost_lower+"lable"+ count[name]).show()
+      input_cost = $("#"+lower_name+cost_lower+"input").clone().attr('id',lower_name+cost_lower+"input"+ count[name]).attr('name',lower_name+'cost'+count[name]).prop('required',true);
+      $("#"+"menuoptions"+lower_name).append(input_cost)
+      $("#"+lower_name+cost_lower+"input"+ count[name]).show()
+    }
+    console.log(data)
 };
 
-function minusbutton(a,b,cost_options){
+function minusbutton(a,b="",cost_options){
   // when the minus button is presed minus one to the amount of the order 
-  
     if(a in count){
       if(count[a] >= 1){
         count[a] -= 1
@@ -70,24 +106,22 @@ function minusbutton(a,b,cost_options){
 x = count[a] + 1
 delete  data[a+x]; 
     document.getElementById(a).innerHTML = count[a];
-
-
-
-console.log(count)
     y = count[a] +1
-    console.log(y)
-
-
-
+    // hides the options when the minus button is press
 for(i in b){
-console.log("working")
 j = b[i].toLowerCase().replaceAll(" ",'_')
  $('#'+j+"input"+y).hide()
  $('#'+j+"lable"+y).hide()
 }
+// hides the cost options when button presed 
 for(i in cost_options){
-  cost_lower = cost_options[i].toLowerCase().replaceAll(" ","_")
+  a_lower = a.toLowerCase().replaceAll(" ","_").replaceAll("$","").replaceAll(".","")
+  cost_lower = cost_options[i].toLowerCase().replaceAll(" ","_").replaceAll("$","").replaceAll(".","")
+  $("#"+a_lower+cost_lower+"lable"+y).hide()
+  $("#"+a_lower+cost_lower+"input"+y).hide()
 } 
+
+  console.log(data)
 };
 function cancelbutton(){
   // cancels all data and returns all data to zero in count and on the page
@@ -101,24 +135,50 @@ function cancelbutton(){
       }
   }
   data = {}
+  $("input[type=radio]").prop('checked', false);
 };
+
+function prosessdata(datas){
+  
+  for (i in datas){
+$("#reciet").append('<p>'+datas[i]+'</p>');
+  }
+}
 
 function orderssubmit(){
   // when the submit button is presed on the /orders page give name to the flask 
   x = {}
   x["Name"] = document.getElementById("OrderName").value;
+  // post to /orders/name
   $.ajax({
     type:"POST",
     data: JSON.stringify(x),
     url:"/orders/name",
     contentType: "application/json",
-    dataType: "json"
+    dataType: "json",
+    success: function(datas){
+      prosessdata(datas)
+    }
   })
-
-
 };
 
-function checkbox(options,name){
+function checkbox(options,name,type){
   //lets check boxes do stuff
-  
-}
+  options_lower = options.toLowerCase().replaceAll(" ","_").replaceAll("$","").replaceAll(".","")
+  name_lower = name.toLowerCase().replaceAll(" ","_").replaceAll("$","").replaceAll(".","")
+
+    if($("#"+name_lower+options_lower+"input"+count[name]).is(':checked') === true){
+      for(i=1;i<= count[name];i++ ){
+        data[name+i]['Cost'] = options;
+        console.log(data)
+      }
+    }
+  console.log("#"+options_lower+"input"+count[name])
+  if($("#"+options_lower+"input"+count[name]).is(':checked') === true){
+    console.log("isworking")
+    for(i=1;i<= count[name];i++ ){
+        data[name+i]['Options'] = options;
+        console.log(data)
+    }
+  }
+};
